7708fe85f070c2b09c40076a5479fc0acfbae6d8
Vitalii <fame.sunset@gmail.com>
2019-10-14 16:40:54 +0300

    add functions structures to Data_Layer

--- Data_Layer/Event.cs
+++ Data_Layer/Event.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,20 @@ namespace Data_Layer
 {
     class Event
     {
+        /// <summary>
+        /// Get data from view to put it into the db.
+        /// </summary>
+        /// <param name="id_User">id of User</param>
+        /// <param name="id_Calendar">id of Calendar</param>
+        /// <param name="Title">Title of the Event</param>>
+        /// <param name="Description">Description of the Event</param>
+        /// <param name="EventTimeStart">The event starts at this time (day, hours and minutes)</param>
+        /// <param name="EventTimeFinish">The Event starts at this time (day, hours and minutes)</param>
+        /// <param name="EventPeriodStart">Datetime of the event when it is started (day period since)</param>
+        /// <param name="EventPeriodFinish">Datetime of the event when it is finished (day period till)</param>
+        public void GetEventData(int id_User, int id_Calendar, string Title, string Description, DateTime EventTimeStart, DateTime EventTimeFinish, DateTime EventPeriodStart, DateTime EventPeriodFinish)
+        {
+            
+        }
     }
 }
---
+++ Data_Layer/Notification.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Layer
+{
+    class Notification
+    {
+        /// <summary>
+        /// Get data from view to put it into the db
+        /// </summary>
+        /// <param name="id_EventSchedule">id of the Event entity</param>
+        /// <param name="NotificationTime">Datetime of the Event entity when notification has to be appeared</param>
+        public void GetNotificationData(int id_EventSchedule, DateTime NotificationTime)
+        {
+
+        }
+    }
+}
