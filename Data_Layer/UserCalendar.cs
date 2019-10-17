0927e51b09a57cc3f0622002181f71e2e404d3df
Vitalii <fame.sunset@gmail.com>
2019-10-17 13:54:34 +0300

    SetCalendarToUser void and sp

--- Data_Layer/UserCalendar.cs
+++ Data_Layer/UserCalendar.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Security.AccessControl;
 using System.Text;
@@ -7,9 +10,28 @@ using System.Threading.Tasks;
 
 namespace Data_Layer
 {
-    class UserCalendar
+    public class UserCalendar
     {
         public int id_User { get; set; }
         public int id_Calendar { get; set; }
+        public UserCalendar(int idUser, int idCalendar)
+        {
+            this.id_Calendar = idCalendar;
+            this.id_User = id_User;
+        }
+
+        public void SetCalendarToUser(UserCalendar userCalendar)
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder
+            {
+                DataSource = "20.188.35.217",
+                UserID = "sa",
+                Password = "Sunsetfame05!",
+                InitialCatalog = "Calendar"
+            };
+            SqlConnection connection = new SqlConnection(builder.ConnectionString);
+            var AddEvent = connection.Query<UserCalendar>("SetCalendarToUser", new { userCalendar.id_Calendar, userCalendar.id_User },
+                commandType: CommandType.StoredProcedure);
+        }
     }
 }
