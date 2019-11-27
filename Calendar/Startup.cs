4a1f0c31c4b57e2598f189c0706c886f7777a944
TheHACKATHON <okorokmahjonga@gmail.com>
2019-11-27 10:42:19 +0200

    Добавил Hangfire

--- Calendar/Startup.cs
+++ Calendar/Startup.cs
@@ -14,8 +14,10 @@ using Business.Services.User;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.Hosting;
-using Newtonsoft.Json;
 using Data.Repository;
+using Hangfire;
+using System;
+using Hangfire.SqlServer;
 
 namespace Calendar
 {
@@ -91,13 +93,30 @@ namespace Calendar
                     options.ClaimActions.MapJsonKey("picture", "picture");
                 });
 
-            services.Configure<BrotliCompressionProviderOptions>(options => { options.Level = CompressionLevel.Optimal; });
+            services.Configure<BrotliCompressionProviderOptions>(options => { options.Level = CompressionLevel.Fastest; });
             services.AddResponseCompression(options =>
             {
                 options.Providers.Add<BrotliCompressionProvider>();
                 options.EnableForHttps = true;
             });
             
+            // Add Hangfire services.
+            services.AddHangfire(configuration => configuration
+                .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseRecommendedSerializerSettings()
+                .UseSqlServerStorage(Configuration.GetConnectionString("HangfireConnection"), new SqlServerStorageOptions
+                {
+                    CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+                    SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+                    QueuePollInterval = TimeSpan.Zero,
+                    UseRecommendedIsolationLevel = true,
+                    UsePageLocksOnDequeue = true,
+                    DisableGlobalLocks = true
+                }));
+
+            // Add the processing server as IHostedService
+            services.AddHangfireServer();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -121,6 +140,10 @@ namespace Calendar
             app.UseAuthorization();
 
             app.UseResponseCompression();
+
+             // hangfire
+            app.UseHangfireDashboard();
+
 
             app.UseEndpoints(endpoints =>
             {
