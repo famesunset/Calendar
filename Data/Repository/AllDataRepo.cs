a98f3a1b07fadb00ac28c73f55d876c7d990d325
TheHACKATHON <okorokmahjonga@gmail.com>
2019-11-28 14:57:26 +0200

    Выравнивание событий

--- Data/Repository/AllDataRepo.cs
+++ Data/Repository/AllDataRepo.cs
@@ -39,7 +39,7 @@ namespace Data.Repository
             using (SqlConnection connection = new SqlConnection(ConnectionString))
             {
                 AllData s = connection.Query<AllData>("uspGetEventById", new { eventId },
-                    commandType: CommandType.StoredProcedure).First();
+                    commandType: CommandType.StoredProcedure).SingleOrDefault();
                 return s;
             }
         }
